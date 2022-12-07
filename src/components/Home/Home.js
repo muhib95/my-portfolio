408071b1f33fba4da9b8f73eae872132f74e6b4b
Muhibbul hasan <muhibbulhasan6@gmail.com>
2022-12-07 14:22:47 +0600

    add header

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import './Home.css'
 const Home = () => {
     return (
-        <div>
-            Home
-            
+        <div className='p-4'>
+           
+<h2>muhii</h2>
         </div>
     );
 };
